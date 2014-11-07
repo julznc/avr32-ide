d9741f1f316047c2f3f020a3652e9b37a85d88c1
julznc <yus.projectproto@gmail.com>
2014-11-07 18:09:26 +0800

    missing header; remove some unused files

---
+++ libraries/Grbl/stepper.h
@@ -0,0 +1,106 @@
+/*
+  stepper.h - stepper motor driver: executes motion plans of planner.c using the stepper motors
+  Part of Grbl v0.9
+
+  Copyright (c) 2012-2014 Sungeun K. Jeon
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/* 
+  This file is based on work from Grbl v0.8, distributed under the 
+  terms of the MIT-license. See COPYING for more details.  
+    Copyright (c) 2009-2011 Simen Svale Skogsrud
+    Copyright (c) 2011 Sungeun K. Jeon
+*/ 
+
+#ifndef stepper_h
+#define stepper_h 
+
+#ifndef SEGMENT_BUFFER_SIZE
+  #define SEGMENT_BUFFER_SIZE 6
+#endif
+
+// Stores the planner block Bresenham algorithm execution data for the segments in the segment 
+// buffer. Normally, this buffer is partially in-use, but, for the worst case scenario, it will
+// never exceed the number of accessible stepper buffer segments (SEGMENT_BUFFER_SIZE-1).
+// NOTE: This data is copied from the prepped planner blocks so that the planner blocks may be
+// discarded when entirely consumed and completed by the segment buffer. Also, AMASS alters this
+// data for its own use. 
+typedef struct {  
+  uint8_t direction_bits;
+  uint32_t steps[N_AXIS];
+  uint32_t step_event_count;
+} st_block_t;
+
+// Primary stepper segment ring buffer. Contains small, short line segments for the stepper 
+// algorithm to execute, which are "checked-out" incrementally from the first block in the
+// planner buffer. Once "checked-out", the steps in the segments buffer cannot be modified by 
+// the planner, where the remaining planner block steps still can.
+typedef struct {
+  uint16_t n_step;          // Number of step events to be executed for this segment
+  uint8_t st_block_index;   // Stepper block data index. Uses this information to execute this segment.
+  uint16_t cycles_per_tick; // Step distance traveled per ISR tick, aka step rate.
+  #ifdef ADAPTIVE_MULTI_AXIS_STEP_SMOOTHING
+    uint8_t amass_level;    // Indicates AMASS level for the ISR to execute this segment
+  #else
+    uint8_t prescaler;      // Without AMASS, a prescaler is required to adjust for slow timing.
+  #endif
+} segment_t;
+
+// Stepper ISR data struct. Contains the running data for the main stepper ISR.
+typedef struct {
+  // Used by the bresenham line algorithm
+  uint32_t counter_x,        // Counter variables for the bresenham line tracer
+           counter_y, 
+           counter_z;
+  #ifdef STEP_PULSE_DELAY
+    uint8_t step_bits;  // Stores out_bits output to complete the step pulse delay
+  #endif
+  
+  uint8_t execute_step;     // Flags step execution for each interrupt.
+  uint8_t step_pulse_time;  // Step pulse reset time after step rise
+  uint8_t step_outbits;         // The next stepping-bits to be output
+  uint8_t dir_outbits;
+  #ifdef ADAPTIVE_MULTI_AXIS_STEP_SMOOTHING
+    uint32_t steps[N_AXIS];
+  #endif
+
+  uint16_t step_count;       // Steps remaining in line segment motion  
+  uint8_t exec_block_index; // Tracks the current st_block index. Change indicates new block.
+  st_block_t *exec_block;   // Pointer to the block data for the segment being executed
+  segment_t *exec_segment;  // Pointer to the segment being executed
+} stepper_t;
+
+// Segment preparation data struct. Contains all the necessary information to compute new segments
+// based on the current executing planner block.
+typedef struct {
+  uint8_t st_block_index;  // Index of stepper common data block being prepped
+  uint8_t flag_partial_block;  // Flag indicating the last block completed. Time to load a new one.
+
+  float steps_remaining;
+  float step_per_mm;           // Current planner block step/millimeter conversion scalar
+  float req_mm_increment;
+  float dt_remainder;
+  
+  uint8_t ramp_type;      // Current segment ramp state
+  float mm_complete;      // End of velocity profile from end of current planner block in (mm).
+                          // NOTE: This value must coincide with a step(no mantissa) when converted.
+  float current_speed;    // Current speed at the end of the segment buffer (mm/min)
+  float maximum_speed;    // Maximum speed of executing block. Not always nominal speed. (mm/min)
+  float exit_speed;       // Exit speed of executing block (mm/min)
+  float accelerate_until; // Acceleration ramp end measured from end of block (mm)
+  float decelerate_after; // Deceleration ramp start measured from end of block (mm)
+} st_prep_t;
+
+#endif
