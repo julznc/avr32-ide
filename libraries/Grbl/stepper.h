60110a1cec4b11f8752ba4fd5e0480230fa1349a
yus <yus.projectproto@gmail.com>
2014-12-23 14:31:47 +0800

    added StepperMotor class

--- libraries/Grbl/stepper.h
+++ libraries/Grbl/stepper.h
@@ -17,35 +17,35 @@
   You should have received a copy of the GNU General Public License
   along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
 */
-/* 
-  This file is based on work from Grbl v0.8, distributed under the 
-  terms of the MIT-license. See COPYING for more details.  
+/*
+  This file is based on work from Grbl v0.8, distributed under the
+  terms of the MIT-license. See COPYING for more details.
     Copyright (c) 2009-2011 Simen Svale Skogsrud
     Copyright (c) 2011 Sungeun K. Jeon
-*/ 
+*/
 
 #ifndef stepper_h
-#define stepper_h 
+#define stepper_h
 
 #ifndef SEGMENT_BUFFER_SIZE
   #define SEGMENT_BUFFER_SIZE 6
 #endif
 
-// Stores the planner block Bresenham algorithm execution data for the segments in the segment 
+// Stores the planner block Bresenham algorithm execution data for the segments in the segment
 // buffer. Normally, this buffer is partially in-use, but, for the worst case scenario, it will
 // never exceed the number of accessible stepper buffer segments (SEGMENT_BUFFER_SIZE-1).
 // NOTE: This data is copied from the prepped planner blocks so that the planner blocks may be
 // discarded when entirely consumed and completed by the segment buffer. Also, AMASS alters this
-// data for its own use. 
-typedef struct {  
+// data for its own use.
+typedef struct {
   uint8_t direction_bits;
   uint32_t steps[N_AXIS];
   uint32_t step_event_count;
 } st_block_t;
 
-// Primary stepper segment ring buffer. Contains small, short line segments for the stepper 
+// Primary stepper segment ring buffer. Contains small, short line segments for the stepper
 // algorithm to execute, which are "checked-out" incrementally from the first block in the
-// planner buffer. Once "checked-out", the steps in the segments buffer cannot be modified by 
+// planner buffer. Once "checked-out", the steps in the segments buffer cannot be modified by
 // the planner, where the remaining planner block steps still can.
 typedef struct {
   uint16_t n_step;          // Number of step events to be executed for this segment
@@ -62,9 +62,9 @@ typedef struct {
 typedef struct {
   // Used by the bresenham line algorithm
   uint32_t counter_x,        // Counter variables for the bresenham line tracer
-           counter_y, 
+           counter_y,
            counter_z;
-  
+
   uint8_t execute_step;     // Flags step execution for each interrupt.
   uint8_t step_pulse_time;  // Step pulse reset time after step rise
   uint8_t step_outbits;         // The next stepping-bits to be output
@@ -73,7 +73,7 @@ typedef struct {
     uint32_t steps[N_AXIS];
   #endif
 
-  uint16_t step_count;       // Steps remaining in line segment motion  
+  uint16_t step_count;       // Steps remaining in line segment motion
   uint8_t exec_block_index; // Tracks the current st_block index. Change indicates new block.
   st_block_t *exec_block;   // Pointer to the block data for the segment being executed
   segment_t *exec_segment;  // Pointer to the segment being executed
@@ -89,7 +89,7 @@ typedef struct {
   float step_per_mm;           // Current planner block step/millimeter conversion scalar
   float req_mm_increment;
   float dt_remainder;
-  
+
   uint8_t ramp_type;      // Current segment ramp state
   float mm_complete;      // End of velocity profile from end of current planner block in (mm).
                           // NOTE: This value must coincide with a step(no mantissa) when converted.
@@ -99,5 +99,16 @@ typedef struct {
   float accelerate_until; // Acceleration ramp end measured from end of block (mm)
   float decelerate_after; // Deceleration ramp start measured from end of block (mm)
 } st_prep_t;
+
+class StepperMotor
+{
+  volatile uint32_t _p1, _p2, _p3, _p4;
+  volatile uint8_t _pos; // 0 - 3
+public:
+  StepperMotor() : _p1(0), _p2(0), _p3(0), _p4(0), _pos(0) { }
+  void init(uint32_t pin1, uint32_t pin2, uint32_t pin3, uint32_t pin4);
+  void idle();
+  void step(int forward=1);
+};
 
 #endif
